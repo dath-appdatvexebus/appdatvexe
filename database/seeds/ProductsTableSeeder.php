38dc802b9ebfa539cca6430d958aac8f9969f73d
hoanghai <lamhoanghai2509@gmail.com>
2020-06-05 23:19:21 +0700

    test config mysql

--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -11,6 +11,14 @@ class ProductsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Product::create([
+            'product_name' => 'MacBook Pro',
+            'product_description' => 'Apple Laptop'
+        ]);
+
+        Product::create([
+            'product_name' => 'Huawei GR5 2017',
+            'product_description' => 'Andriod Smart Phone'
+        ]);
     }
 }
