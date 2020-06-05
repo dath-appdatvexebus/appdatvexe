41328fef6a190d91462463c390647c773205a95c
hoanghai <lamhoanghai2509@gmail.com>
2020-06-05 22:47:08 +0700

    test config mysql

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'us-cdbr-east-05.cleardb.net'),
 
     /*
     |--------------------------------------------------------------------------
@@ -46,22 +46,23 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'us-cdbr-east-05.cleardb.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'heroku_9a3fc1b2fc2f89f'),
+            'username' => env('DB_USERNAME', 'b5a52638e2389d'),
+            'password' => env('DB_PASSWORD', '06a865d8'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
             'prefix_indexes' => true,
-            'strict' => true,
+            'strict' => false,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
+
 
         'pgsql' => [
             'driver' => 'pgsql',
@@ -91,6 +92,7 @@ return [
             'prefix_indexes' => true,
         ],
 
+    
     ],
 
     /*
