1151caf2405e4a27e5366f8c8ab4c4d7fe3e1f87
hai2509 <lamhoanghai2509@gmail.com>
2020-06-27 10:50:43 +0700

    update test web

---
+++ build/precache-manifest.bdbdf85dbc8118c2767f965893f7b1d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b02481e11a77eeaad4aee4a15151858",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd83b95bb8ad9f22442f",
+    "url": "/static/css/2.adb28eb3.chunk.css"
+  },
+  {
+    "revision": "ad142bcfce6c4361680f",
+    "url": "/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "cd83b95bb8ad9f22442f",
+    "url": "/static/js/2.c3d3aca2.chunk.js"
+  },
+  {
+    "revision": "e0546182649f50a2eb923c2a881ed896",
+    "url": "/static/js/2.c3d3aca2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad142bcfce6c4361680f",
+    "url": "/static/js/main.d8314cff.chunk.js"
+  },
+  {
+    "revision": "84f1be50c96141b366fd",
+    "url": "/static/js/runtime-main.33d27179.js"
+  }
+]);
